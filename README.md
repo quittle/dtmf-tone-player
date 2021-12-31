88565430361bc2674ee5affdc32ccb57df167a24
Dustin Toff <me@dustintoff.com>
2021-12-31 12:19:00 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,9 @@
+# DTMF Tone Player
+
+Learn to identify "Dual tone multi frequency" notes used by phones
+
+![Screenshot](./screenshot.png)
+
+# Build and Usage
+
+No build necessary, just open [`tones.html`](./tones.html).
--- custom.js
+++ custom.js
@@ -2,34 +2,36 @@ function setCurrentTone(value) {
     document.getElementById('current-tone').innerText = value;
 }
 
+let pressing=false;
+function press(num) {
+    const buttons = document.querySelectorAll('table button');
+    const button = Array.from(buttons).find(button => button.innerText == num);
+    if (button == null || pressing) {
+        return;
+    }
+    pressing = true;
+    button.onmousedown();
+    setTimeout(() => {
+        button.onmouseup();
+        pressing = false;
+    }, 1000);
+}
+
 function init() {
     document.addEventListener('keypress', e => {
-        if (e.key === ' ') {
+        const key = e.key;
+        if (key === ' ') {
             setCurrentTone('?');
             const max = parseInt(document.getElementById('max-tone').value);
             const num = Math.floor(Math.random() * (max + 1));
+
             press(num);
             setTimeout(() => setCurrentTone(num), 1000);
         } else {
-            setCurrentTone(e.key);
-            press(e.key);
+            setCurrentTone(key);
+            press(key);
         }
     });
-
-    var pressing=false;
-    function press(num) {
-        var buttons = document.querySelectorAll('table button');
-        var button = Array.from(buttons).find(button => button.innerText == num);
-        if (button == null || pressing) {
-            return;
-        }
-        pressing = true;
-        button.onmousedown();
-        setTimeout(() => {
-            button.onmouseup();
-            pressing = false;
-        }, 1000);
-    }
 }
 
 init();
